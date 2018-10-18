b688115abf724088bdb44793fdb440972c4ed4d8
Gabriel Possemozer <gossemozer2012@gmail.com>
2018-10-18 15:31:40 -0300

    Criando entity post com User aninhado

--- src/main/java/com/possemozergabriel/workshopmongo/service/UserService.java
+++ src/main/java/com/possemozergabriel/workshopmongo/service/UserService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.possemozergabriel.workshopmongo.domain.User;
+import com.possemozergabriel.workshopmongo.dto.UserDTO;
 import com.possemozergabriel.workshopmongo.repository.UserRepository;
+import com.possemozergabriel.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,37 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repo.findOne(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = repo.findOne(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
